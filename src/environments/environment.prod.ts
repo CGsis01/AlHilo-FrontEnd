f5c4d1405bbf7c51020aeda6b65f705cb5a20d02
César Ortega <sistemas@grupocorpcg.com>
2026-03-11 14:40:52 -0600

    Fix files to deploy in Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Environment configuration for production
 export const environment = {
   production: true,
-  apiUrl: 'https://api.alhilo.com/api/v1',  // Update with your production API URL
+  apiUrl: 'https://al-hilo-back-end.vercel.app/api/v1',  // Update with your production API URL
   apiTimeout: 30000,
   tokenKey: 'accessToken',
   refreshTokenKey: 'refreshToken',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 // Environment configuration for development
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.68.113:8000/api/v1',  // FastAPI default port
+  // apiUrl: 'http://192.168.68.116:8000/api/v1',  // FastAPI default port
+  apiUrl: 'https://al-hilo-back-end.vercel.app/api/v1',  // FastAPI default port
   apiTimeout: 30000,  // 30 seconds
   tokenKey: 'accessToken',
   refreshTokenKey: 'refreshToken',
