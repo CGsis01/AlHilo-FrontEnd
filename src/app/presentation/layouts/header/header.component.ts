a60db30993df79fe43bac49924761defd1f502e6
sistemas1-grupocg <sistemas1@grupocorpcg.com>
2026-04-21 11:40:09 -0600

    Services were added to the receptionist's role, and a bug was fixed w… (#1)
    * Services were added to the receptionist's role, and a bug was fixed where deactivating an item caused the items to move out of place
    
    * changes of test
    
    * changes that had been discussed
    
    * changes that had been discussed
    
    * delet comment

--- src/app/presentation/layouts/header/header.component.ts
+++ src/app/presentation/layouts/header/header.component.ts
@@ -64,10 +64,10 @@ export class HeaderComponent implements OnChanges {
           roles: [UserRoleCode.ADMIN, UserRoleCode.RECEPTIONIST],
           children: [
             { label: 'Clientes', route: '/customers', icon: '👤', roles: [UserRoleCode.ADMIN, UserRoleCode.RECEPTIONIST] },
-            { label: 'Materiales', route: '/materials', icon: '📦', roles: [UserRoleCode.ADMIN, UserRoleCode.SUPERADMIN] },
+            { label: 'Materiales', route: '/materials', icon: '📦', roles: [UserRoleCode.ADMIN, UserRoleCode.SUPERADMIN, UserRoleCode.RECEPTIONIST] },
             { label: 'Complejidades', route: '/repair-complexities', icon: '⚙️', roles: [UserRoleCode.ADMIN, UserRoleCode.SUPERADMIN] },
-            { label: 'Tipos de compostura', route: '/repair-types', icon: '📋', roles: [UserRoleCode.ADMIN] },
-            { label: 'Prendas', route: '/garments', icon: '👔', roles: [UserRoleCode.ADMIN, UserRoleCode.SUPERADMIN] }
+            { label: 'Tipos de compostura', route: '/repair-types', icon: '📋', roles: [UserRoleCode.ADMIN, UserRoleCode.RECEPTIONIST] },
+            { label: 'Prendas', route: '/garments', icon: '👔', roles: [UserRoleCode.ADMIN, UserRoleCode.SUPERADMIN, UserRoleCode.RECEPTIONIST] }
           ]
         },
         { 
